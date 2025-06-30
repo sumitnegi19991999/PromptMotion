2b6ad32b32b878a278ec793e4ec982527f464e82
Sumit Negi <sumitnegi19991999@gmail.com>
2025-06-30 16:16:44 +0530

    add:ai

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,24 @@
+import {   openai, createAgent } from "@inngest/agent-kit";
+
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+  async ({ event }) => {
+     // Create a new agent with a system prompt (you can add optional tools, too)
+     const manimAgent = createAgent({
+        name: "manim-agent",
+        system: "You are an expert Python developer who writes code to create animation using manim library. you write simple mantanable code that create smooth and perfect animation using manin library.you write manim code ",
+        model: openai({ model: "gpt-4o" }),
+      }); 
+    // await step.sleep("wait-a-moment", "5s");
+    const { output } = await manimAgent.run(
+        `Write the following code : ${event.data.value}`,
+      );
+      console.log(output);
+      // [{ role: 'assistant', content: 'function removeUnecessaryWhitespace(...' }]
+    return {  output };
   },
 );
